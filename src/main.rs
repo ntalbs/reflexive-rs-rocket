2b055ce9ba78b4519711ab1909a90d712979d7d0
ntalbs <ntalbsen@gmail.com>
2023-03-11 17:23:10 +0000

    Echo back query parameters

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,55 @@
 use std::collections::BTreeMap;
 
 use rocket::{Route, Request, Data, route};
-use serde::{Serialize, ser::SerializeStruct};
+use serde::{Serialize, ser::{SerializeStruct, SerializeSeq}};
+
+enum SingleOrMulti {
+    Single(String),
+    Multi(Vec<String>),
+}
+
+impl<'a> Serialize for SingleOrMulti {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        match self {
+            SingleOrMulti::Single(v) => serializer.serialize_str(v),
+            SingleOrMulti::Multi(vs) => {
+                let mut seq = serializer.serialize_seq(Some(vs.len()))?;
+                for e in vs {
+                    seq.serialize_element(e)?;
+                }
+                seq.end()
+            }
+        }
+    }
+}
+
+fn querys_as_map(req: &Request) -> BTreeMap<String, SingleOrMulti> {
+    let mut ret = BTreeMap::new();
+
+    for q in req.query_fields() {
+        let k = q.name.to_string();
+        let v = q.value.to_string();
+        match ret.get_mut(&k) {
+            None => {
+                ret.insert(k, SingleOrMulti::Single(v));
+            }
+            Some(SingleOrMulti::Single(ov)) => {
+                let vs = vec![ov.clone(), v];
+                ret.insert(k, SingleOrMulti::Multi(vs));
+            }
+            Some(SingleOrMulti::Multi(vs)) => {
+                vs.push(v);
+            }
+        }
+    }
+
+    ret
+}
 
 struct EchoResponse {
     method: String,
     path: String,
-    queries: BTreeMap<String, String>,
+    queries: BTreeMap<String, SingleOrMulti>,
     headers: BTreeMap<String, String>,
     body: String,
 }
@@ -31,7 +74,7 @@ fn echo<'a>(req: &'a Request, _: Data<'a>) -> route::BoxFuture<'a> {
     let echo = EchoResponse {
         method: req.method().to_string(),
         path: req.uri().path().to_string(),
-        queries: BTreeMap::new(),
+        queries: querys_as_map(&req), //BTreeMap::new(),
         headers: BTreeMap::new(),
         body: "BODY".to_string(),
     };
